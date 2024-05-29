24bf07501aa3c3ddc61302cab1a77414b8024a11
Renke Brixel <93048457+ReynkeDeVos@users.noreply.github.com>
2024-05-29 14:26:14 +0200

    chore: Update PokeCard component to fix image import
    The PokeCard component was modified to fix the image import path. The image source was updated to use the correct path for the shoe image.

--- Frontend/src/components/PokeCard.jsx
+++ Frontend/src/components/PokeCard.jsx
@@ -1,13 +1,12 @@
-import { useState } from "react";
-
-import "./App.css";
+import "../App.css";
 
 function PokeCard() {
-
   return (
     <>
       <div className="card w-96 bg-base-100 shadow-xl">
-        <figure><img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+        <figure>
+          <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
+        </figure>
         <div className="card-body">
           <h2 className="card-title">
             Shoes!
@@ -15,7 +14,7 @@ function PokeCard() {
           </h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
           <div className="card-actions justify-end">
-            <div className="badge badge-outline">Fashion</div> 
+            <div className="badge badge-outline">Fashion</div>
             <div className="badge badge-outline">Products</div>
           </div>
         </div>
